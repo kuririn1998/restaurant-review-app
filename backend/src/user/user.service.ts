fa988606bbdd8209394d7a056e74a0cca56dfb74
ise-208 <yt.iwase09@gmail.com>
2024-10-04 02:36:24 +0900

    loginエンドポイントの追加

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -8,7 +8,8 @@ export class UserService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
-  ) {}
+  ) {
+  }
 
   async createUser(
     username: string,
@@ -17,5 +18,15 @@ export class UserService {
   ): Promise<User> {
     const newUser = this.userRepository.create({ username, email, password });
     return this.userRepository.save(newUser);
+  }
+
+  async loginUser(email: string, password: string): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (!user) {
+      throw new UnauthorizedException('このユーザーは存在しません。');
+    } else if (user.password != password) {
+      throw new UnauthorizedException('パスワードが違います。');
+    }
+    return user;
   }
 }
